b691bbe864d0c989277e308c3ad25bffdddea456
Rohit Rout <rohitrout@outlook.com>
2023-10-15 18:55:03 +0530

    signIn/signUp form && login

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+const Header = () => {
+  return (
+    <div className="absolute bg-gradient-to-b from-black z-10">
+      <img
+        className="w-56"
+        src="https://images.ctfassets.net/y2ske730sjqp/1aONibCke6niZhgPxuiilC/2c401b05a07288746ddf3bd3943fbc76/BrandAssets_Logos_01-Wordmark.jpg?w=940"
+        alt="logo"
+      />
+    </div>
+  );
+};
+
+export default Header;
